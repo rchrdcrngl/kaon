4e5f230dfff8de963b0e27f5195647219be3e431
Richard Maru Caringal <richard.caringal23@gmail.com>
2024-08-16 15:37:14 +0800

    initial code for registry & users service

---
+++ api/srvcregistry/src/main/java/com/kaon/srvcregistry/SrvcregistryApplication.java
@@ -0,0 +1,13 @@
+package com.kaon.srvcregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvcregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvcregistryApplication.class, args);
+	}
+
+}
---
+++ api/srvcregistry/src/test/java/com/kaon/srvcregistry/SrvcregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaon.srvcregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvcregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
