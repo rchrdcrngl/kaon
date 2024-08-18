9e6822e5521f4a04d7e3044749220655f7cce491
Richard Maru Caringal <richard.caringal23@gmail.com>
2024-08-19 03:22:43 +0800

    working eureka server dotnet & java

--- api/gateway/src/main/java/com/kaon/gateway/ApiGatewayApplication.java
+++ api/gateway/src/main/java/com/kaon/gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.kaon.users;
+package com.kaon.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UsersApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UsersApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
--- api/srvcregistry/src/main/java/com/kaon/srvcregistry/SrvcregistryApplication.java
+++ api/srvcregistry/src/main/java/com/kaon/srvcregistry/SrvcregistryApplication.java
@@ -2,8 +2,10 @@ package com.kaon.srvcregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SrvcregistryApplication {
 
 	public static void main(String[] args) {
